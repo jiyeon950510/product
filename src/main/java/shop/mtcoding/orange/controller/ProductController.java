2709879edc7a8ef5a53ea36f85d3b8beeb54432a
jiyeon950510 <siki4682@naver.com>
2023-01-17 12:42:14 +0900

    mybatis 상품 프로그램 완료

---
+++ src/main/java/shop/mtcoding/orange/controller/ProductController.java
@@ -0,0 +1,114 @@
+package shop.mtcoding.orange.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import shop.mtcoding.orange.model.Product;
+import shop.mtcoding.orange.model.ProductRepository;
+
+@Controller
+public class ProductController {
+
+    @Autowired // Type 으로 찾아냄
+    private ProductRepository productRepository;
+
+    @Autowired
+    private HttpSession session;
+
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+
+    @GetMapping("/redirest")
+    public void redirest(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // HttpSession session = request.getSession();
+        session.setAttribute("name", "seession metacoding");
+        request.setAttribute("name", "metacoding");
+        response.sendRedirect("test");
+    }
+
+    @GetMapping("/dispatcher")
+    public void dispatcher(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.setAttribute("name", "metacoding");
+        RequestDispatcher dis = request.getRequestDispatcher("/test");
+        dis.forward(request, response);
+    }
+
+    @GetMapping({ "/", "/product" })
+    public String findAllProduct(Model model) {
+        List<Product> productList = productRepository.findAll();
+        model.addAttribute("productList", productList);
+        return "product/main"; // request 새로 만들어짐 -> 덮어씌움. (프레임워크)
+    }
+
+    @GetMapping("/product/{id}")
+    public String apiFindOneProduct(@PathVariable int id, Model model) {
+        Product product = productRepository.findOne(id);
+        model.addAttribute("product", product);
+        return "product/detail";
+    }
+
+    @GetMapping("/main")
+    public String orange() {
+        return "product/main";
+    }
+
+    @GetMapping("/product/addForm")
+    public String addForm() {
+        return "/product/addForm";
+    }
+
+    @PostMapping("/product/add")
+    public String add(String name, int price, int qty) {
+        int result = productRepository.insert(name, price, qty);
+        if (result == 1) {
+            return "redirect:/product";
+        } else {
+            return "redirect:/product/addForm";
+        }
+    }
+
+    @PostMapping("/product/{id}/delete")
+    public String delete(@PathVariable int id) {
+        int result = productRepository.delete(id);
+
+        if (result == 1) {
+            return "redirect:/product";
+        } else {
+            return "redirect:/product/" + id;
+        }
+    }
+
+    @GetMapping("/product/{id}/updateForm")
+    public String updateForm(@PathVariable int id, Model model) {
+        Product product = productRepository.findOne(id);
+        model.addAttribute("product", product);
+        return "product/updateForm";
+    }
+
+    @PostMapping("/product/{id}/update")
+    public String update(@PathVariable int id, String name, int price, int qty) {
+        int result = productRepository.update(id, name, price, qty);
+        if (result == 1) {
+            return "redirect:/product/" + id;
+        } else {
+            return "product/{id}/update" + id + "/updateForm";
+        }
+    }
+
+}
